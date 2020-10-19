08afa9c624ba49eff4b6fa35e381fe960b398103
清雪冰橙 <1154238323@qq.com>
2020-10-19 21:00:42 +0800

    2020/10/19 21:00

--- hpsp-web/src/main/java/com/shock/hpsp/web/Main.java
+++ hpsp-web/src/main/java/com/shock/hpsp/web/Main.java
@@ -1,15 +1,32 @@
 package com.shock.hpsp.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author czf
  * @Date 2020/10/13 1:41 下午
  */
 @SpringBootApplication
+@RestController
+@Api("MainController")
 public class Main {
+
+
+    @RequestMapping("/test")
+    public String h(@ApiParam(name="param", value = "参数") String param){
+        return "input:"+param;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
     }
 }
---
+++ hpsp-web/src/main/java/com/shock/hpsp/web/config/Swagger2Configuration.java
@@ -0,0 +1,39 @@
+package com.shock.hpsp.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author czf
+ * @Date 2020/10/19 5:51 下午
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+    public static  final String SWAGGER_SCAN_BASE_PACKAGE = "com.shock.hpsp.web";
+    public static final String VERSION = "1.0.0";
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("单词计数服务") //设置文档的标题
+                .description("单词计数服务 API 接口文档") // 设置文档的描述
+                .version(VERSION) // 设置文档的版本信息-> 1.0.0 Version information
+                .termsOfServiceUrl("http://www.baidu.com") // 设置文档的License信息->1.3 License information
+                .build();
+    }
+}
