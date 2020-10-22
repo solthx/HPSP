1bbe76219814fa2b075ed8395ee26f150c15c113
清雪冰橙 <1154238323@qq.com>
2020-10-22 22:43:29 +0800

    2020/10/22

--- hpsp-web/src/main/java/com/shock/hpsp/web/Main.java
+++ hpsp-web/src/main/java/com/shock/hpsp/web/Main.java
@@ -3,8 +3,10 @@ package com.shock.hpsp.web;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +21,18 @@ import org.springframework.web.multipart.MultipartFile;
 @Api("MainController")
 public class Main {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("/test")
     public String h(@ApiParam(name="param", value = "参数") String param){
+        redisTemplate.boundValueOps("test").set(param);
+
+        System.out.println(redisTemplate.opsForValue().get("test"));
         return "input:"+param;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
 }
